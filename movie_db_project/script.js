def4553f522ff7a22f594f29e0e44f536ac65e43
ZeyuChen777 <134183679+ZeyuChen777@users.noreply.github.com>
2023-06-04 23:08:11 -0500

    Add files via upload

--- movie_db_project/script.js
+++ movie_db_project/script.js
@@ -1,36 +1,3 @@
-// * There should be a header showing icon and two tabs "HOME" and "LIKED LIST"
-// * The TAB should show an active style for current active tab
-// * There should be a dropdown select with categories "Popular", "Now playing", "Top rated" and "Up coming"
-// * The app should load the "Popular" movies by default.
-// * Each page should have 20 movie card, 4 card in a row
-// * Each movie card should show the movie poster, movie title, movie rating with a star icon and outlined like icon
-// * Clicking the "like" icon will turn the icon from outlined to solid contained red.
-// * Hovering on the movie title will turn title to another color
-// * Clicking the movie title in the card will open a movie detail modal
-// * The movie detail modal has a semi black transparent backdrop effect
-// * The movie detail modal should show movie poster, movie title, movie overview, movie genres, movie rating and movie production companies logos
-// * There should a close icon on the top right allowing the user to close the modal.
-// * There should be a pagination section with a "prev", a "next" button and a text of current page and total page
-// * Clicking on the next button will load next page of movies for current category
-// * Clicking on the prev button will load prev page of movies for current category
-// * The perv button should be disabled when it is the first page
-// * The next button should be disabled when it is the last page
-// * Choosing other category from the drop down should load the first page of movies for the selected category
-// * Clicking on the "LIKED LIST" in the header should list all liked movies
-// * Clicking the like button of the already like movie again should remove it from the like list
-
-// Resources:
-// Movie details: https://developers.themoviedb.org/3/movies/get-movie-details
-// Now playing movies: https://developers.themoviedb.org/3/movies/get-now-playing
-// Popular movies: https://developers.themoviedb.org/3/movies/get-popular-movies
-// Top rated movies: https://developers.themoviedb.org/3/movies/get-top-rated-movies
-// Up coming movies: https://developers.themoviedb.org/3/movies/get-upcoming
-// logo & colors: https://www.themoviedb.org/about/logos-attribution
-// rating icon color: #f5c518
-// icons: https://ionicons.com/v4/
-
-//console.log(document.querySelector("iframe"));
-
 const API_KEY = "af3ad269dc54a8aacf2c6204fc737af1";
 
 const BASE_URL = "https://api.themoviedb.org/3";
@@ -58,7 +25,7 @@ function getMovies(){
   let category = model.currentFilter,
       page = model.currentPage;
   let url = `${BASE_URL}/movie/${category}?page=${page}&api_key=${API_KEY}`;
-  console.log(category);
+  //console.log(category);
   fetch(url)
     .then(res => res.json())
     .then(data => {model.movieList = data.results; 
@@ -224,6 +191,7 @@ const closeModal = () => {
   const modal = document.querySelector("#modal");
   modal.style.display = "none";
 };
+
 //listClickHandler
 function listClickHandler(e){
   const target = e.target;
